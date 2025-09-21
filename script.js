94567628bca2b5715840e5fb72849e012a364d93
Shiori572 <shiori5721154@gmail.com>
2025-09-22 01:14:59 +0900

    main

---
+++ script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor:true,
+  spaceBetween:30,
+
+  navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+  },
+
+
+  
+  breakpoints:{
+   0:{
+    slidesPerView:1
+   },
+   620:{
+    slidesPerView:2
+   },
+   1024:{
+    slidesPerView:3
+   }
+
+  }
+});
